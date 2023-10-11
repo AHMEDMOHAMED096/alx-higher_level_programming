da9b559bbc9a9afa0aee54ac999a795faa9e0bf1
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-10-11 12:19:08 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,9 +3,9 @@
 from add_0 import add
 
 def add1():
-a = 1
-b = 2
-print("{} + {} = {} ".format(a, b, add(a, b))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b))
 
 
 if __name__ = "__main__":
