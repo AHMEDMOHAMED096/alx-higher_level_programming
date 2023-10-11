2a738d91f50bbf79fa6a6204139995ee7b858322
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-10-11 12:58:24 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    args_num = len(args)
+
+    if args_num == 1:
+        print("Number of argument: {}".format(args_num))
+    else:
+        print("Number of arguments: {}".format(args_num))
+
+    for i in range(args_num):
+        print("{}: {}".format(i + 1, args[i]))
