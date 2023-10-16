ef1373189785b58981a33b1bae4dcb9b24cee0d2
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-10-16 15:42:02 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     output = (len(sentence), sentence[0])
     if sentence == "":
-        return sentence[0] = None
+        return (0, None)
     else:
         return output
