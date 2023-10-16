7f3b6aedc10e2f3e6264543e7a5eb6fd53ac843d
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-10-16 08:50:52 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for char in range(122, 96, -1):
-    if char % 2 == 0:
-        print(chr(char), end="")
-    elif char % 2 == 1:
-        print(chr(char - 32), end="")
+    print("{}".format(chr(char if char % 2 == 0 else char - 32)), end="")
+
