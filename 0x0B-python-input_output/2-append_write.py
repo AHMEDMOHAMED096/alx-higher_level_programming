9bf3b46007f47f8e33d00b077c819e7eaf49bb61
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-11-07 13:03:27 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,14 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of UTF-8 file"""
+    """Appends a string at the end of UTF-8 file
+
+    Parameters:
+    filename: The name of file to append to
+    text: The string to append to file
+
+    Return: The number of characters added"""
+
     with open(filename, "a", encoding="utf-8") as my_file:
-        count = my_file.append(text)
+        count = my_file.write(text)
     return count
