5abccfcb8f2bcc852ee4053caef3b2d2e90fa699
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-11-08 08:06:08 -0800

    delete

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines a file reading function"""
-
-
-def read_file(filename=""):
-    """Prints the content of UTF-8 file to stdout"""
-    with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read(), end="")
--- 0x0B-python-input_output/1-write_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines a text file writing function"""
-
-
-def write_file(filename="", text=""):
-    """Writes a string to UTF-8 file"""
-    with open(filename, "w", encoding="utf-8") as my_file:
-        count = my_file.write(text)
-    return count
