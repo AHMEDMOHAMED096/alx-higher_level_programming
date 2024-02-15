9731a7bff1947e2d3ae4e7e28850611e998863b0
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-14 16:32:50 -0800

    fixed syntax error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -46,9 +46,9 @@ class Rectangle:
             ValueError: If width is less than 0.
         """
 
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -75,8 +75,8 @@ class Rectangle:
             ValueError: If width is less than 0.
         """
 
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
