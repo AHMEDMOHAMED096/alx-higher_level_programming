6dfff1962bd35fcd4d06fc7492c33a553c359c03
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-19 14:09:04 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,8 +4,7 @@
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object
-
-    Return:
-        list: obj
     """
-    return dir(obj)
+    mylist = dir(obj)
+
+    return mylist
