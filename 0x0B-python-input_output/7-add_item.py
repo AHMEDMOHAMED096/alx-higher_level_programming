b6013916ef77fe81afa5f53635f6a278bfde0b54
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-20 10:25:29 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
-""" imported sys module
-    imported 5-save_to_json_file module
-    imported 6-load_from_json_file """
+""" Write a script that adds all arguments to a Python list
+and then save them to a file. """
 import sys
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
-""" Write a script that adds all arguments to a Python list
-and then save them to a file. """
 
 
 try:
