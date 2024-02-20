711d32827cf7744b01ea49e20e80de0d1f7537a6
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-20 07:29:06 -0800

    created repo && solve tasks

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines a function for reading a file. """
+
+
+def read_file(filename=""):
+    """ A function to read a text file. """
+
+    with open("", encoding="utf-8") as myfile:
+        myfile.read()
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines a function to write a string to a text file. """
+
+
+def write_file(filename="", text=""):
+    """ A function to write a string to a text file. """
+    with open("", "w") as myfile:
+        myfile.write("", "")
