a3f77da55bccfbc3c81309a28e1d8638cb74c285
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-20 10:18:43 -0800

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" imported json module. """
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+""" Write a script that adds all arguments to a Python list
+and then save them to a file. """
+
+
+try:
+    items = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, 'add_item.json')
