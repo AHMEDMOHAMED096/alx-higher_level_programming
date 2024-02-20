8595bc995d9018359865f7815b9aac56b33b663b
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-20 10:27:34 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Write a script that adds all arguments to a Python list
+""" A script that adds all arguments to a Python list
 and then save them to a file. """
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+if __name__ == "__main__":
+    from 5-save_to_json_file import save_to_json_file
+    from 6-load_from_json_file import load_from_json_file
 
 
 try:
