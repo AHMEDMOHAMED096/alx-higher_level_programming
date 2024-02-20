ec1ac1432df32a709cbaf67f81a524de277869d7
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-20 09:13:20 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ A function to read a text file. """
 
-    with open(filename, encoding="utf-8") as myfile:
+    with open(filename, encoding="UTF-8") as myfile:
         print(myfile.read())
