8690d42c2c0d96b3c386dcf63b0aeebdbdacaf61
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-20 09:45:33 -0800

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
+import json
 """ Defines a function that returns the JSON representation of an object. """
 
 
 def to_json_string(my_obj):
     """ A function that returns the JSON representation of an object. """
-    return str(json.dumps(my_obj))
+    return json.dumps(my_obj)
