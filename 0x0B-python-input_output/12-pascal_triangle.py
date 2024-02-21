7833b38dac9fe4a472286beb208f68856fff3006
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-21 13:51:55 -0800

    fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,17 +5,14 @@
 def pascal_triangle(n):
     """ A function that returns the Pascal’s triangle.
     Returns a list of lists of integers. """
-
     triangle = [[1]]
     if n <= 0:
         return []
     for i in range(1, n):
         last_element = triangle[-1]
         new_list = [1]
-    for j in range(len(last_element) - 1):
-        new_list.append(last_element[j] + last_element[j + 1])
-    new_list.append(1)
-    triangle.append(new_list)
-
-
-return triangle
+        for j in range(len(last_element) - 1):
+            new_list.append(last_element[j] + last_element[j + 1])
+        new_list.append(1)
+        triangle.append(new_list)
+    return triangle
