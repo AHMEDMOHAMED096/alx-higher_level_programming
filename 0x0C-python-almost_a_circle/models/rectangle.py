923afa0fa09ad84717b4d11b7601a8322d30cd00
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2024-02-25 20:02:46 +0200

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,138 @@
 #!/usr/bin/python3
 Base = __import__('base').Base
+''' Defines a class named Rectangle. '''
+
 
 class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        pass
+        '''
+        The constructor for the Rectangle class.
+
+        Parameters:
+            width: The width of Rectangle.
+            height: The height of Rectangle.
+            x: set to 0 by default.
+            y: set to 0 by default.
+        '''
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """
+        This is a getter method for the width attribute.
+
+        Returns:
+            int: The width of rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        This is a setter method for the width attribute.
+
+         Parameters:
+            value (int): The new width of the Rectangle.
+
+        Raises:
+            TypeError: If width is not an integer.
+            ValueError: If width is less than 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        This is a getter method for the height attribute.
+
+        Returns:
+            int: The height of Rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        This is a setter method for the height attribute.
+
+         Parameters:
+            value (int): The new height of the Rectangle.
+
+        Raises:
+            TypeError: If height is not an integer.
+            ValueError: If height is less than 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        This is a getter method for the x attribute.
+
+        Returns:
+            int: x of Rectangle
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        This is a setter method for the x attribute.
+
+         Parameters:
+            value (int): The new x of the Rectangle.
+
+        Raises:
+            TypeError: If x is not an integer.
+            ValueError: If x is less than 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        This is a getter method for the y attribute.
+
+        Returns:
+            int: y of Rectangle
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        This is a setter method for the y attribute.
+
+         Parameters:
+            value (int): The new y of the Rectangle.
+
+        Raises:
+            TypeError: If y is not an integer.
+            ValueError: If y is less than 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__y = value
