e643013975b69a4f8dc068ef9610dc54d146f2c8
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2024-02-26 23:49:29 +0200

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -157,16 +157,16 @@ class Rectangle(Base):
         """
         This method returns nicely readable string representation of an object.
         """
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__x}/{self.__y} - \
-{self.__width}/{self.__height}")
+        return (f"[{self.__class__.__name__}] ({self.id}) \
+{self.__x}/{self.__y} - {self.__width}/{self.__height}")
 
     def update(self, *args, **kwargs):
         self.id = args[0] if len(args) > 0 else kwargs.get('id', self.id)
         self.width = args[1] if len(args) > 1 \
-        else kwargs.get('width', self.width)
+            else kwargs.get('width', self.width)
 
         self.height = args[2] if len(args) > 2 \
-        else kwargs.get('height', self.height)
+            else kwargs.get('height', self.height)
 
         self.x = args[3] if len(args) > 3 else kwargs.get('x', self.x)
         self.y = args[4] if len(args) > 4 else kwargs.get('y', self.y)
