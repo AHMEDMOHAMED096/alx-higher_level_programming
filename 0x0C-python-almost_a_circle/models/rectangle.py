127729f4ef551d5ff522ce01f0006a41cc65cf8d
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2024-02-26 21:24:27 +0200

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -163,8 +163,8 @@ class Rectangle(Base):
         ''' This method assigns an argument to each attribute. '''
         self.id = args[0] if len(args) > 0 else kwargs.get('id', self.id)
         self.width = args[1] if len(args) > 1 \
-else kwargs.get('width', self.width)
+        else kwargs.get('width', self.width)
         self.height = args[2] if len(args) > 2 \
-else kwargs.get('height', self.height)
+        else kwargs.get('height', self.height)
         self.x = args[3] if len(args) > 3 else kwargs.get('x', self.x)
         self.y = args[4] if len(args) > 4 else kwargs.get('y', self.y)
