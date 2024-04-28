136a37bedc2f36c32afec3c61ea1d64641d40dff
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-04-28 06:01:51 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,30 +3,34 @@
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(
+
+def list_states(username, password, database_name):
+    """Connect to the MySQL server"""
+    conn = MySQLdb.connect(
         host="localhost",
         user=username,
         passwd=password,
         db=database_name,
         port=3306
-        )
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
 
-states = cursor.fetchall()
+    for state in states:
+        print(state)
 
-for state in states:
-    print(state)
+    cursor.close()
+    conn.close()
 
-cursor.close()
-conn.close()
 
 if __name__ == "__main__":
-    # Check if all three arguments are provided
+    """Check if all three arguments are provided"""
     if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database_name>")
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
+
+    list_states(username, password, database_name)
