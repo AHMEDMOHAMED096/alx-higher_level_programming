0776bef1bf9b208bb2fd3f9ccb37d5fb9a7e519d
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-05-10 16:33:22 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ url = 'https://alx-intranet.hbtn.io/status'
 
 with request.urlopen(url) as response:
     body = response.read()
-
+    print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
     print("\t- utf8 content: {}".format(body.decode('utf-8')))
