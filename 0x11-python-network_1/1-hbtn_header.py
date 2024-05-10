51704e6ecd630f1cdea8d3867040a8e6e8058b85
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-05-10 16:38:24 -0700

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Import urllib and sys
+from urllib import request
+import sys
+
+url = sys.argv[1]
+
+with request.urlopen(url) as response:
+    headers = response.getheaders()
+
+    for header in headers:
+        if header[0] == 'X-Request-Id':
+            print(header[1])
