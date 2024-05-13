4c15fc3ba1851dd973d984911d76eb25a40ab03d
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-05-13 06:17:51 +0300

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@ import requests
 url = "https://alx-intranet.hbtn.io/status"
 
 response = requests.get(url)
+content = response.text
 print("Body response:")
-print("\t- type: {}".format(type(url)))
-print("\t- content: {}".format(response.content))
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
