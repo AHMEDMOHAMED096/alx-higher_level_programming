f641bca87dcb5e0578d59bb49b0bdf45a27f136e
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-05-13 06:46:35 +0300

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Import necessary modules """
+import requests
+import sys
+
+
+def send_post_request(url, email):
+    """
+    This function takes a URL and an email as input and
+    sends a POST request to the URLwith the email as a parameter
+    and prints the body of the response.
+    """
+
+    response = requests.post(url, data={"email": email})
+    print("Your email is:", response.text)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    send_post_request(url, email)
