776589862a4e15ef34ab93f9af1176f861908138
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-06-05 02:16:06 +0300

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/* script that prints the number of movies where the character “Wedge Antilles” is present */
+const request = require('request');
+
+const url = process.argv[2];
+
+const options = {
+  url,
+  method: 'GET',
+  json: true
+};
+
+request(options, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: Unexpected status code ${response.statusCode}`);
+    process.exit(1);
+  } else {
+    const films = body.results;
+    const characterID = 'https://swapi-api.alx-tools.com/api/people/18/';
+    let count = 0;
+
+    films.forEach(film => {
+      if (film.characters.includes(characterID)) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  }
+});
